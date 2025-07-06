2d41ba283602795b341c7464bfed4f7290564538
FAFernandez247 <felicityfernandez247@gmail.com>
2025-07-06 15:28:02 +0800

    test registraion activity

--- tests/features/registration.spec.js
+++ tests/features/registration.spec.js
@@ -5,21 +5,39 @@ test.beforeEach(async ({ page }) => {
     await page.getByRole('link', { name: 'Admin Page' }).click();
     await page.getByRole('button', { name: 'Clean' }).click();
 });
-test('Verifythat user is able to register account', async ({ page }) => {
+test('Verify that user is able to register account', async ({ page }) => {
   await page.goto('https://parabank.parasoft.com/parabank/index.htm');
   await page.getByRole('link', { name: 'Register' }).click();
   await page.locator('[id="customer.firstName"]').fill('Feli');
   await page.locator('[id="customer.lastName"]').fill('Fernan');
   await page.locator('[id="customer.address.street"]').fill('123 main st');
-  await page.locator('[id="customer.address.city"]').fill('test city');
-  await page.locator('[id="customer.address.state"]').fill('test state');
-  await page.locator('[id="customer.address.zipCode"]').fill('1234');
-  await page.locator('[id="customer.phoneNumber"]').fill('09123456789');
-  await page.locator('[id="customer.ssn"]').fill('123456');
-  await page.locator('[id="customer.username"]').fill('TestUN1234');
-  await page.locator('[id="customer.password"]').fill('testun1234');
-  await page.locator('#repeatedPassword').fill('testun1234');
+  await page.locator('[id="customer.address.city"]').fill('imus city');
+  await page.locator('[id="customer.address.state"]').fill('cavite state');
+  await page.locator('[id="customer.address.zipCode"]').fill('0987');
+  await page.locator('[id="customer.phoneNumber"]').fill('09987654321');
+  await page.locator('[id="customer.ssn"]').fill('987654');
+  await page.locator('[id="customer.username"]').fill('FeliTest123');
+  await page.locator('[id="customer.password"]').fill('password4testing');
+  await page.locator('#repeatedPassword').fill('password4testing');
   await page.getByRole('button', { name: 'Register' }).click();
   await expect(page.locator('h1')).toBeVisible();
   await expect(page.locator('h1')).toContainText(/Welcome/);
+});
+
+test('Register with missing fields', async ({ page }) => {
+  await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.locator('[id="customer.firstName"]').fill('Feli');
+  await page.locator('[id="customer.lastName"]').fill('Fernan');
+  await page.locator('[id="customer.address.street"]').fill('test addr');
+  await page.locator('[id="customer.address.city"]').fill('test city');
+  await page.locator('[id="customer.address.state"]').fill('test state');
+  await page.locator('[id="customer.username"]').fill('FeliTest123');
+  await page.locator('[id="customer.password"]').fill('password4testing');
+  await page.locator('#repeatedPassword').fill('password4testing');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByText('Zip Code is required.')).toBeVisible();
+  await expect(page.getByRole('cell', { name: 'Social Security Number is' })).toBeVisible();
+  await expect(page.locator('h1')).toBeVisible();
+  await expect(page.locator('h1')).toContainText('Signing up is easy!');
 });
